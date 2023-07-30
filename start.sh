3eb94e8e2b12da1820ea23ef97149e0de7785ba2
Thang Vo <vmthang914@gmail.com>
2023-07-30 21:57:48 +0700

    Signed-off-by: Thang Vo <vmthang914@gmail.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+docker build -t hadoop-base docker/hadoop/hadoop-base && \
+docker build -t hive-base docker/hive/hive-base && \
+docker build -t spark-base docker/spark/spark-base && \
 docker-compose -f docker-compose-CeleryExecutor.yml up -d
